79f929c878a50eccffc2e7a7de2bf77dee7cc1d7
Rafael <rafaeladonisf @gmail.com>
2024-03-09 20:27:09 -0500

    terminando la barra de progreso

--- src/app/components/increase/increase.component.ts
+++ src/app/components/increase/increase.component.ts
@@ -1,23 +1,42 @@
 import { CommonModule, I18nPluralPipe } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-increase',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
+
   templateUrl: './increase.component.html',
   styleUrl: './increase.component.css',
 })
 export class IncreaseComponent implements OnInit {
-  @Input() btnClass: string = 'btn-danger';
-  @Input() progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
+  @Input('valorEntrada') progreso: number = 50;
   @Output() valorSalida = new EventEmitter<number>();
   ngOnInit(): void {
     this.btnClass = `btn ${this.btnClass}`;
   }
 
   increase(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      return;
+    } else if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
+      return;
+    }
     this.progreso += valor;
+    this.valorSalida.emit(this.progreso);
+  }
+  onChange(nuevoValor: number) {
+    if (nuevoValor >= 100) {
+      this.progreso = 100;
+    } else if (nuevoValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
     this.valorSalida.emit(this.progreso);
   }
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -8,4 +8,15 @@ import { IncreaseComponent } from '../../components/increase/increase.component'
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.css',
 })
-export class ProgressComponent {}
+export class ProgressComponent {
+  progreso1: number = 10;
+  progreso2: number = 10;
+
+  get getProgreso1() {
+    return `${this.progreso1}%`;
+  }
+
+  get getProgreso2() {
+    return `${this.progreso2}%`;
+  }
+}
